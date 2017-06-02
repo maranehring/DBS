56e24c92d82943ed8e79bbe13ec83f4371656ad7
maranehring <m.k.nehring@web.de>
2017-06-02 12:44:26 +0200

    Create All.py

---
+++ All.py
@@ -0,0 +1,26 @@
+'''import csv
+import psycopg2
+
+conn_string = "dbname= 'election' user = 'postgres' password = 'password'"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
+
+reader = csv.reader(open('american-electin-tweets.csv'.'rb'))
+
+for row in reader:
+    print row [1]
+
+    statement = "INSERT INTO product_template (handle,tweet, "
+'''
+
+from One import func1
+from Two import func2
+from Three import func3
+
+def main():
+    func1()
+    func2()
+    func3()
+
+if __name__=='__main__':
+    main()
