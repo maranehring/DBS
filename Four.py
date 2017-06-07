1446fc449413bdedc3c3c6c93a8ab520114c47db
maranehring <m.k.nehring@web.de>
2017-06-07 12:10:39 +0200

    Update Four.py

--- Four.py
+++ Four.py
@@ -1,9 +1,8 @@
-import csv
-import unicodecsv as csv
+import unicodecsv
 def func4():
-    with open('american-election-tweets.csv', 'r') as inp, open('first_file.csv', 'w') as out:
-        writer = csv.writer(out, delimiter=';', lineterminator='\n')
-        stuff = csv.reader(inp, delimiter=';')
+    with open('third_file.csv', 'r') as inp, open('last_file.csv', 'w') as out:
+        writer = unicodecsv.writer(out, delimiter=';', lineterminator='\n', encoding='ISO-8859-1')
+        stuff = unicodecsv.reader(inp, delimiter=';', encoding='ISO-8859-1')
 
         all = []
         row = next(stuff)
