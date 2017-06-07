63ccff7aae8502bcafdc0a57e4c692ad1e85f9c4
maranehring <m.k.nehring@web.de>
2017-06-07 12:13:49 +0200

    Update All.py

--- All.py
+++ All.py
@@ -1,28 +1,13 @@
-'''import csv
-import psycopg2
-
-conn_string = "dbname= 'election' user = 'postgres' password = 'password'"
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor()
-
-reader = csv.reader(open('american-electin-tweets.csv'.'rb'))
-
-for row in reader:
-    print row [1]
-
-    statement = "INSERT INTO product_template (handle,tweet, "
-'''
-
 from One import func1
 from Two import func2
 from Three import func3
 from Four import func4
 
 def main():
-    func4()
     func1()
     func2()
     func3()
+    func4()
 
 if __name__=='__main__':
     main()
